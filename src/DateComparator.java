fea0d35ff61fabbc13d63ffddf2d03016758a519
Tomasz Turek <turek9933@gmail.com>
2026-04-19 01:10:12 +0200

    Cleaning old files

--- src/DateComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class DateComparator implements Comparator<Event> {
-	@Override
-	public int compare(Event e1, Event e2) {
-		return e1.getDate().compareTo(e2.getDate());
-	}
-}
--- src/PhoneNumberComperator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class PhoneNumberComperator implements Comparator<Contact> {
-	@Override
-	public int compare(Contact c1, Contact c2) {
-		return c1.getPhoneNumber() - c2.getPhoneNumber();
-	}
-}
